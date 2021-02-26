1e4fb1f80ae97f74ca73f174468bc7063c88358e
Aia Rupsom <aiale88@gmail.com>
2021-02-26 11:10:50 +0200

    use modules to structure ts files in the project

--- public/app.js
+++ public/app.js
@@ -1,28 +1,19 @@
-"use strict";
-// classes
-var Invoice = /** @class */ (function () {
-    function Invoice(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
-    Invoice.prototype.format = function () {
-        return this.client + " owes \u00A3" + this.amount + " for " + this.details;
-    };
-    return Invoice;
-}());
-var invOne = new Invoice('mario', 'work on the mario website', 250);
-var invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-var invoices = [];
+import { Invoice } from './classes/invoice.js';
+const invOne = new Invoice('mario', 'work on the mario website', 250);
+const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-var form = document.querySelector('.new-item-form');
+invoices.forEach((inv) => {
+    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
+});
+const form = document.querySelector('.new-item-form');
 // inputs
-var type = document.querySelector('#type');
-var tofrom = document.querySelector('#tofrom');
-var details = document.querySelector('#details');
-var amount = document.querySelector('#amount');
-form.addEventListener('submit', function (e) {
+const type = document.querySelector('#type');
+const tofrom = document.querySelector('#tofrom');
+const details = document.querySelector('#details');
+const amount = document.querySelector('#amount');
+form.addEventListener('submit', (e) => {
     e.preventDefault();
     console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
 });
