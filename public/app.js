c198966f589f1174e1187b1d261f5593095207d1
Aia Rupsom <aiale88@gmail.com>
2021-02-26 11:50:21 +0200

    create first interface and payment class

--- public/app.js
+++ public/app.js
@@ -1,13 +1,14 @@
-import { Invoice } from './classes/invoice.js';
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
-let invoices = [];
-invoices.push(invOne);
-invoices.push(invTwo);
-invoices.forEach((inv) => {
-    console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
+import { Invoice } from './classes/Invoice.js';
+import { Payment } from './classes/Payment.js';
+let docOne;
+let docTwo;
+docOne = new Invoice('yoshi', 'web work', 250);
+docTwo = new Payment('mario', 'plumbing', 200);
+let docs = [];
+docs.push(docOne);
+docs.push(docTwo);
 const form = document.querySelector('.new-item-form');
+console.log(form.children);
 // inputs
 const type = document.querySelector('#type');
 const tofrom = document.querySelector('#tofrom');
@@ -15,5 +16,12 @@ const details = document.querySelector('#details');
 const amount = document.querySelector('#amount');
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    console.log(type.value, tofrom.value, details.value, amount.valueAsNumber);
+    let doc;
+    if (type.value === 'invoice') {
+        doc = new Invoice(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    else {
+        doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
+    }
+    console.log(doc);
 });
